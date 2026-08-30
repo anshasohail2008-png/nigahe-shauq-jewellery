8da3ad89cf833307b866daedc005b6cc1e3ad2ca
anshasohail2008-png <anshasohail2008@gmail.com>
2026-08-30 15:56:27 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   base: '/nigahe-shauq-jewellery/',
   plugins: [react()],
